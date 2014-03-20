9751bbfa5a89663463a7b6138df0ebbb7d922157
TeodorDumitru <TeoDum@gmail.com>
2014-03-20 10:30:42 +0100

    Created Reservation class, ReservationMapper and ReservationMapperInterface.

---
+++ src/domain/Reservation.java
@@ -0,0 +1,60 @@
+package domain;
+
+import java.sql.Date;
+
+public class Reservation {
+
+    private int ID;
+    private int roomID;
+    private int customerID;
+    private Date checkinDate;
+    private int numberNights;
+
+    public Reservation(int ID, int roomID, int customerID, Date checkinDate, int numberNights) {
+        this.ID = ID;
+        this.roomID = roomID;
+        this.customerID = customerID;
+        this.checkinDate = checkinDate;
+        this.numberNights = numberNights;
+    }
+
+    public int getID() {
+        return ID;
+    }
+
+    public void setID(int ID) {
+        this.ID = ID;
+    }
+
+    public int getRoomID() {
+        return roomID;
+    }
+
+    public void setRoomID(int roomID) {
+        this.roomID = roomID;
+    }
+
+    public int getCustomerID() {
+        return customerID;
+    }
+
+    public void setCustomerID(int customerID) {
+        this.customerID = customerID;
+    }
+
+    public Date getCheckinDate() {
+        return checkinDate;
+    }
+
+    public void setCheckinDate(Date checkinDate) {
+        this.checkinDate = checkinDate;
+    }
+
+    public int getNumberNights() {
+        return numberNights;
+    }
+
+    public void setNumberNights(int numberNights) {
+        this.numberNights = numberNights;
+    }
+}
