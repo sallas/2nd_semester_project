48a9a95e75d3b20fc191275bd2d47b331d8da05b
TeodorDumitru <TeoDum@gmail.com>
2014-03-20 12:09:28 +0100

    Changed DBConnector, created DBFacade.

--- src/dataSource/DBFacade.java
+++ src/dataSource/DBFacade.java
@@ -1,5 +1,25 @@
 package dataSource;
 
+import domain.Reservation;
+
 public class DBFacade {
 
+    private static ReservationMapperInterface rm;
+    private static DBFacade instance;
+
+    private DBFacade() {
+        rm = new ReservationMapper(DBConnector.getConnection());
+    }
+
+    public static DBFacade getInstance() {
+        return instance;
+    }
+
+    public Reservation getReservation(int ID) {
+        return rm.getReservation(ID);
+    }
+
+    public boolean saveReservatoin(Reservation r) {
+        return rm.saveReservation(r);
+    }
 }
