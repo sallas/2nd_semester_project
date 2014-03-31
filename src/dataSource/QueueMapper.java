168efc72c0043058d37e01f64f8a2235effe319e
Мартин Гергов <martingergov1@gmail.com>
2014-03-31 14:32:48 +0200

    Queue classes and test added.

---
+++ src/dataSource/QueueMapper.java
@@ -0,0 +1,12 @@
+package dataSource;
+
+import java.sql.Connection;
+
+public class QueueMapper extends AbstractMapper{
+
+    public QueueMapper(Connection con) {
+        super(con);
+    }
+    
+    
+}
---
+++ test/dataSource/QueueMapperTest.java
@@ -0,0 +1,20 @@
+package dataSource;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class QueueMapperTest {
+    
+    public QueueMapperTest() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+}
